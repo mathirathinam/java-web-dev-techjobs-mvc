b387641961d2f8dce923f35d88dc2af6375714de
mathirathinam <mathirathinam2015@gmail.com>
2021-03-04 02:48:50 -0600

    changes-committed

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import static org.launchcode.javawebdevtechjobsmvc.controllers.ListController.columnChoices;
 
@@ -23,6 +24,19 @@ public class SearchController {
         return "search";
     }
 
-    // TODO #3 - Create a handler to process a search request and render the updated search view.
+    // TODO #3 - Create a handler to process a search request and render the updated search view.findByColumnAndValue(String column, String value)
+    @PostMapping ("results")
+    public String displaySearchResults (Model model, @RequestParam String searchTerm, @RequestParam String searchType) {
+        ArrayList<Job> jobs;
+        if (searchType.toLowerCase().equals("all") ){
+            jobs = JobData.findAll();
+            model.addAttribute("title", "All Jobs");
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("title", "Jobs with " + columnChoices.get(searchType) + ": " + searchTerm);
+        }
+        model.addAttribute("jobs", jobs);
 
+        return "search";
+    }
 }
